4dbfcb78a6de7e7ad2e02a0adb6d75cd8d6b5dfe
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2024-12-02 21:23:14 +1300

    Add exception for rocket part recipe
    Don't skip checking hidden from player crafting recipes. These are still usable by players

--- angelsdev-unit-test/unit-tests/unit-test-003.lua
+++ angelsdev-unit-test/unit-tests/unit-test-003.lua
@@ -4,6 +4,7 @@ local unit_test_functions = require("unit-test-functions")
 
 local recipes_to_ignore = {
   ["electric-energy-interface"] = true, -- base game
+  ["rocket-part"] = true, -- base game. Recipe is hidden from player crafting but item is hidden
 }
 local products_to_ignore = {
   ["chemical-void"] = true,
@@ -18,7 +19,7 @@ local unit_test_003 = function()
   local item_prototypes = prototypes.item
   local fluid_prototypes = prototypes.fluid
   for recipe_name, recipe_data in pairs(recipe_prototypes) do
-    if not recipe_data.hidden and not recipe_data.hide_from_player_crafting and not recipes_to_ignore[recipe_name] then -- only test recipes that are not hidden or ignored
+    if not recipe_data.hidden and not recipes_to_ignore[recipe_name] then -- only test recipes that are not hidden or ignored
       local recipe_ingredients = recipe_data.ingredients
       for _, recipe_ingredient in pairs(recipe_ingredients) do
         if recipe_ingredient.type == "item" then
